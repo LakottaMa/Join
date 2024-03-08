e6de58bc800562004b5f9bcc1f4e1b74b08953bf
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-03-08 19:02:23 +0100

    add subtask function

--- js/add_tasks.js
+++ js/add_tasks.js
@@ -1,3 +1,9 @@
+let subTasks = [];
+
+// document.getElementById('addSubTaskBtn').addEventListener("click", () => {
+//     document.getElementById('subtaskInput').focus();
+// });
+
 /**
  * toggle display: 'none'; to show or hide element
  * @param {string} id 
@@ -12,10 +18,10 @@ function toggleCustomSelect(id) {
  * @param {string} setId set the text to the id
  */
 function selectElement(getId, setId) {
-   let choice = document.getElementById(getId).innerText;
-   document.getElementById(setId).innerHTML = `${choice}`;
-   let cat = setId == 'selectedCategory' ? 'taskCategory' : 'userCategory';
-   toggleCustomSelect(cat);
+    let choice = document.getElementById(getId).innerText;
+    document.getElementById(setId).innerHTML = `${choice}`;
+    let cat = setId == 'selectedCategory' ? 'taskCategory' : 'userCategory';
+    toggleCustomSelect(cat);
 }
 
 function addTask() {
@@ -25,4 +31,40 @@ function addTask() {
 function showSubtaskInput() {
     document.getElementById('subtaskInput').classList.remove('d-none');
     document.getElementById('subtaskText').classList.add('d-none');
+    document.getElementById('plusIcon').classList.add('d-none');
+    document.getElementById('checkAndCloseIcons').classList.remove('d-none');
+}
+
+function hideSubtaskInput(event) {
+    event.stopPropagation();
+    document.getElementById('checkAndCloseIcons').classList.add('d-none');
+    document.getElementById('subtaskInput').classList.add('d-none');
+    document.getElementById('subtaskText').classList.remove('d-none');
+    document.getElementById('plusIcon').classList.remove('d-none');
+}
+
+function addSubTasks(event) {
+    event.stopPropagation();
+    let input = document.getElementById('subtaskInput');
+    subTasks.push(input.value);
+    input.value = '';
+    renderSubTasks();
+    hideSubtaskInput(event);
+
+}
+
+function renderSubTasks() {
+    let task = document.getElementById('subTaskContainer');
+    task.innerHTML = '';
+
+    for (let i = 0; i < subTasks.length; i++) {
+        const subTask = subTasks[i];
+        task.innerHTML = printSubTasks(subTask);
+    }
+}
+
+function printSubTasks(subTask) {
+    return /*html*/ `
+        <div>${subTask}</div>
+    `;
 }
