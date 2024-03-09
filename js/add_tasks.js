8e402c6126cc56efd8fae84fba011fb0ebfe9635
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-03-09 21:29:32 +0100

    add selected users
    get random color, select and unselect users, print users

--- js/add_tasks.js
+++ js/add_tasks.js
@@ -131,5 +131,147 @@ function showEditSubTaskInputField(index) {
     document.getElementById(`deleteAndEditIcons${index}`).classList.add('d-none');
     document.getElementById(`dividerHorizontal${index}`).classList.remove('d-none');
     document.getElementById(`dividerHorizontal${index}`).style.borderBottomColor = "var(--clr-main2)";
-
 }
+
+/**
+ * render User
+ */
+function renderUsers() {
+    let userContainer = document.getElementById('userCategory');
+    userContainer.innerHTML = '';
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i]['name'];
+        userContainer.innerHTML += printUsers(user, i);
+    }
+}
+
+/**
+ * generate html
+ * @param {string} user from renderUser() function
+ * @param {Int} index from renderUser() function
+ * @returns 
+ */
+function printUsers(user, index) {
+    return /*html*/ `
+        <div class="user" id="user${index}" >
+            <span>${user}</span>
+            <img onclick="selectUser(${index})" id="imgUncheck${index}" src="./assets/img/check_unchecked.png" alt="">
+            <img onclick="unselectUser(${index})" id="imgCheck${index}" class="d-none" src="./assets/img/check_checked.png" alt="">
+        </div>
+    `;
+}
+
+/**
+ * select user with index, push to array and render
+ * @param {Int} index from the user
+ */
+function selectUser(index) {
+    document.getElementById(`imgUncheck${index}`).classList.add('d-none');
+    document.getElementById(`imgCheck${index}`).classList.remove('d-none');
+    let user = users[index]['name'];
+    selectedUsers.push(user);
+    renderSelectedUsers();
+}
+
+/**
+ * unselect user with index, remove from array and render
+ * @param {Int} index from user
+ */
+function unselectUser(index) {
+    document.getElementById(`imgUncheck${index}`).classList.remove('d-none');
+    document.getElementById(`imgCheck${index}`).classList.add('d-none');
+    let user = users[index]['name'];
+    let indexToRemove = selectedUsers.indexOf(user);
+    indexToRemove !== -1 ? selectedUsers.splice(indexToRemove, 1) : console.log('user not found');
+    renderSelectedUsers();
+}
+
+/**
+ * split the string in two strings, get the first letter from each string
+ * @param {string} string to create initials
+ * @returns initials
+ */
+function getInitials(string) {
+    let words = string.split(" ");
+    let initials = "";
+    words.forEach(word => {
+        initials += word.charAt(0);
+    });
+    return initials;
+}
+
+/**
+ * render selected Users and get random background color
+ */
+function renderSelectedUsers() {
+    let container = document.getElementById('selectedUser');
+    container.innerHTML = '';
+    for (let i = 0; i < selectedUsers.length; i++) {
+        const user = selectedUsers[i];
+            container.innerHTML += printSelectedUsers(user, i);
+            document.getElementById(`selectedUser${i}`).style.backgroundColor = getRandomColor();
+    }
+}
+
+/**
+ * generate html from the initials
+ * @param {string} user 
+ * @param {Int} index 
+ * @returns 
+ */
+function printSelectedUsers(user, index) {
+    return /*html*/ `
+        <div id="selectedUser${index}" class="selectedUser">
+            ${getInitials(user)}
+        </div>
+    `;
+}
+
+/**
+ * 
+ * @returns random color from array colors
+ */
+function getRandomColor() {
+    let rndIndex = Math.floor(Math.random() * colors.length);
+    return colors[rndIndex];
+}
+
+let colors = ["#FFC0CB", "#ADD8E6", "#FFFF99", "#98FB98", "#E6E6FA"];
+
+let selectedUsers = [];
+/**
+ * just examples
+ */
+let users = [
+    {
+        "name": "Tim Cook",
+        "email": "tim.cook@example.com",
+        "password": "Cook#Apple5",
+        "tasks": []
+    },
+    {
+        "name": "Steve Jobs",
+        "email": "steve.jobs@example.com",
+        "password": "Jobs#Apple1",
+        "tasks": []
+    },
+    {
+        "name": "Bill Gates",
+        "email": "bill.gates@example.com",
+        "password": "Gates@Microsoft2",
+        "tasks": []
+    },
+    {
+        "name": "Linus Torvalds",
+        "email": "linus.torvalds@example.com",
+        "password": "Torvalds#Linux3",
+        "tasks": []
+    },
+    {
+        "name": "Sam Altman",
+        "email": "sam.altman@example.com",
+        "password": "Altman#YCombinator4",
+        "tasks": []
+    }
+];
+
