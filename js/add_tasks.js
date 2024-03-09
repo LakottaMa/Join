2f962c9d68c73b4716c7d81ab893ddb1a3df950c
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-03-09 19:00:53 +0100

    added eventlistener, delete and edit subtasks

--- js/add_tasks.js
+++ js/add_tasks.js
@@ -1,9 +1,5 @@
 let subTasks = [];
 
-// document.getElementById('addSubTaskBtn').addEventListener("click", () => {
-//     document.getElementById('subtaskInput').focus();
-// });
-
 /**
  * toggle display: 'none'; to show or hide element
  * @param {string} id 
@@ -27,7 +23,9 @@ function selectElement(getId, setId) {
 function addTask() {
     console.log('addTask Button pressed');
 }
-
+/**
+ * show the input field for adding subtasks
+ */
 function showSubtaskInput() {
     document.getElementById('subtaskInput').classList.remove('d-none');
     document.getElementById('subtaskText').classList.add('d-none');
@@ -35,6 +33,10 @@ function showSubtaskInput() {
     document.getElementById('checkAndCloseIcons').classList.remove('d-none');
 }
 
+/**
+ * hide the input field and show text with plus button
+ * @param {event} event prevent the function from the parent
+ */
 function hideSubtaskInput(event) {
     event.stopPropagation();
     document.getElementById('checkAndCloseIcons').classList.add('d-none');
@@ -43,6 +45,10 @@ function hideSubtaskInput(event) {
     document.getElementById('plusIcon').classList.remove('d-none');
 }
 
+/**
+ * get text from input and push to array
+ * @param {event} event prevent the function from the parent
+ */
 function addSubTasks(event) {
     event.stopPropagation();
     let input = document.getElementById('subtaskInput');
@@ -53,18 +59,77 @@ function addSubTasks(event) {
 
 }
 
+/**
+ * render the added subTasks
+ */
 function renderSubTasks() {
     let task = document.getElementById('subTaskContainer');
     task.innerHTML = '';
 
     for (let i = 0; i < subTasks.length; i++) {
         const subTask = subTasks[i];
-        task.innerHTML = printSubTasks(subTask);
+        task.innerHTML += printSubTasks(subTask, i);
     }
 }
 
-function printSubTasks(subTask) {
+/**
+ * create html code
+ * @param {string} subTask from the subTasks array
+ * @returns 
+ */
+function printSubTasks(subTask, index) {
     return /*html*/ `
-        <div>${subTask}</div>
+        <div class="subTaskBox" id="subTaskBox${index}">
+            <span id="subTask${index}">${subTask}</span>
+            <div class="d-none subTaskInput" id="subTaskInput${index}">
+                <div class="subTaskEditBox">
+                    <input id="newSubTask${index}" type="text" placeholder="edit...">
+                    <div class="deleteAndEditIcons" class="deleteAndEditIcons">
+                        <img onclick="deleteSubTask(${index})" src="./assets/img/delete.png" alt="">
+                        <img onclick="saveNewSubTask(${index})" src="./assets/img/check.png" alt="">
+                    </div>
+                </div>
+                <hr id="dividerHorizontal${index}" class="dividerHorizontal d-none">
+            </div>
+           
+            <div id="deleteAndEditIcons${index}" class="deleteAndEditIcons">
+                <img onclick="showEditSubTaskInputField(${index})" src="./assets/img/edit.png" alt="">
+                <hr class="dividerVertical">
+                <img onclick="deleteSubTask(${index})" src="./assets/img/delete.png" alt="">
+            </div>
+        </div>
     `;
+}
+
+/**
+ * delete subtask and render subtasks
+ * @param {Int} index to delete
+ */
+function deleteSubTask(index) {
+    subTasks.splice(index, 1);
+    renderSubTasks();
+}
+
+/**
+ * save edited subtask and render subtasks
+ * @param {Int} index to edit
+ */
+function saveNewSubTask(index) {
+    let newTask = document.getElementById(`newSubTask${index}`);
+    subTasks.splice(index, 1, newTask.value);
+    newTask.value = '';
+    renderSubTasks();
+}
+
+/**
+ * show input field to edit subtask
+ * @param {Int} index for the subtask to edit
+ */
+function showEditSubTaskInputField(index) {
+    document.getElementById(`subTaskInput${index}`).classList.remove('d-none');
+    document.getElementById(`subTask${index}`).classList.add('d-none');
+    document.getElementById(`deleteAndEditIcons${index}`).classList.add('d-none');
+    document.getElementById(`dividerHorizontal${index}`).classList.remove('d-none');
+    document.getElementById(`dividerHorizontal${index}`).style.borderBottomColor = "var(--clr-main2)";
+
 }
