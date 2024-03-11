42a8f57319b165b590dd7af7c5cb8d410062af75
Marcel Lakotta <marcel.lakotta@aol.de>
2024-03-11 19:13:36 +0100

    anything
    /responsive landing page
    / template in borad int
    /fonts klp. int

--- js/summary.js
+++ js/summary.js
@@ -2,13 +2,13 @@ function greetingMessage() {
     let currentHour = checkHour();
     let greeting = document.getElementById('greeting');
     if (currentHour >= 0 && currentHour <= 11) {
-        greeting.innerHTML = 'Good Morning,';
+        greeting.innerHTML = 'Good Morning';
     } else if (currentHour > 11 && currentHour < 14) {
         greeting.innerHTML = 'Good Day,'
     } else if (currentHour >= 14 && currentHour < 18) {
-        greeting.innerHTML = 'Good afternoon,'
+        greeting.innerHTML = 'Good afternoon'
     } else {
-        greeting.innerHTML = 'Good Evening,';
+        greeting.innerHTML = 'Good Evening';
     }
 }
 
