dfdd6d6723d6c3d16b66e1e21998aefdbdffe572
Sebastian Torney <STorney@web.de>
2024-03-12 22:56:51 +0100

    new contact

--- js/contacs.js
+++ js/contacs.js
@@ -1,7 +1,7 @@
 
 function initContacts() {
     renderContactList();
-    console.log('all Contacts Array', allContacts); //wieder löschen!!
+    console.log('all Contacts Array', users); //wieder löschen!!
 }
 
 
@@ -30,9 +30,8 @@ function contactsHTML(contact) {
     let names = contact['name'].split(' '); //map iteriert durch jedes wort im array names
     let initials = names.map(word => word.charAt(0).toUpperCase()).join('');  //join wird verwendet um die elemente des arrays in eine zeichenkette zu verwandeln
     let id = contact['id'];
-
     return `
-        <div class="contactSmall cp" onclick="showFloatContact("${contact})">
+        <div class="contactSmall cp" onclick="showFloatContact('${JSON.stringify(contact)}')">
             <div id="${id}"class="initials">${initials}</div>
             <div>
                 <span>${contact['name']}</span>
@@ -42,16 +41,8 @@ function contactsHTML(contact) {
     `;
 }
 
-function addBgColor(contact) {
-    let id = contact['id'];
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    document.getElementById(`${id}`).style.backgroundColor = bgColor;
-}
-
-function showFloatContact(contact) {
+function showFloatContact(contact) { 
+    
     let name = contact['name'];
     let email = contact['email'];
     let phone = contact['phone'];
@@ -61,33 +52,42 @@ function showFloatContact(contact) {
 
     document.getElementById('floatingContact').classList.remove('d-none');
     document.getElementById('floatingContact').innerHTML ='';
-    document.getElementById('floatingContact').innerHTML =`
+    document.getElementById('floatingContact').innerHTML = floatContactHTML(name, email, phone, id, initials);
+}
+
+function floatContactHTML (name, email, phone, id, initials){
+    return `
         <div class="floatingTop">
-            <div class="initialsFloating">${initials}</div>
+            <div id="${id}" class="initialsFloating">${initials}</div>
             <div class="floatingInteracts">
-            <span>${name}</span>
-            <div class="floatingBtn">
-                <p class="cp" onclick="editContact()"><img src="./assets/img/edit.png" alt="edit">Edit</p>
-                <p class="cp" onclick="deleteContact()"><img src="./assets/img/delete.png" alt="trashcan">Delete</p>
+                <span>${name}</span>
+                <div class="floatingBtn">
+                     <p class="cp" onclick="editContact(${contact})"><img src="./assets/img/edit.png" alt="edit">Edit</p>
+                     <p class="cp" onclick="deleteContact(${contact})"><img src="./assets/img/delete.png" alt="trashcan">Delete</p>
+                </div>
             </div>
         </div>
-</div>
-<div class="floatingBottom">
-    <h2>Contact Information</h2>
-    <div>
-        <h3>Email</h3>
-        <div id="emailFloating">anton@mail.com</div>
-    </div>
-    <div>
-        <h3>Phone</h3>
-        <div id="phoneFloating">+49154627</div>
-    </div>
+        <div class="floatingBottom">
+            <h2>Contact Information</h2>
+        <div>
+            <h3>Email</h3>
+            <div id="emailFloating">${email}</div>
+        </div>
+        <div>
+            <h3>Phone</h3>
+            <div id="phoneFloating">${phone}</div>
+        </div>
 
-</div>
-    
-    
-    `;
+`;
+}
 
+function addBgColor(contact) {
+    let id = contact['id'];
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.getElementById(`${id}`).style.backgroundColor = bgColor;
 }
 
 // function createNewContact() {
