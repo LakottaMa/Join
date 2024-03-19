14149acc89cba338771afc7cee3e75636dd959da
Marcel Lakotta <marcel.lakotta@aol.de>
2024-03-19 17:27:06 +0100

    anything fixed

--- js/summary.js
+++ js/summary.js
@@ -38,14 +38,46 @@ function checkHour() {
 }
 
 function counterSummery(){
-  let tasksInBoard = document.getElementById('tib');
-  let tasksToDo = document.getElementById('todo');
-  for (let i = 0; i < tasks.length; i++) {
-    
-  }
-  const allTasks = `${tasks.length}`;
-  // const allToDo = `${tasks.status['To Do']}`;
+  const tasksInBoard = document.getElementById('tib');
+  const tasksToDo = document.getElementById('todo');
+  const TasksinProgress = document.getElementById('progress');
+  const TasksawFeedback = document.getElementById('awFeedback');
+  const TaskstasksDone = document.getElementById('tasksDone');
+  const Tasksurgent = document.getElementById('urgent');
+  const allTasks = tasks.length;
+  const allToDo = tasks.filter((entry) => entry.status === "To Do").length;
+  const inProgress = tasks.filter((entry) => entry.status === "In progress").length;
+  const awFeedback = tasks.filter((entry) => entry.status === "Await feedback").length;
+  const tasksDone = tasks.filter((entry) => entry.status === "Tasks Done").length;
+  const urgent = tasks.filter((entry) => entry.priority === "Urgent").length;
+  showAllSummaryCounter(tasksInBoard, tasksToDo, TasksinProgress, TasksawFeedback, TaskstasksDone, allTasks, allToDo, inProgress, awFeedback, tasksDone, Tasksurgent, urgent);
+}
+
+function showAllSummaryCounter(tasksInBoard, tasksToDo, TasksinProgress, TasksawFeedback, TaskstasksDone, allTasks, allToDo, inProgress, awFeedback, tasksDone, Tasksurgent, urgent) {
   tasksInBoard.textContent = allTasks;
-  // tasksToDo.textContent = allToDo;
-  
+  tasksToDo.textContent = allToDo;
+  TasksinProgress.textContent = inProgress;
+  TasksawFeedback.textContent = awFeedback;
+  TaskstasksDone.textContent = tasksDone;
+  Tasksurgent.textContent = urgent;
+  nextUrgent();
+}
+ 
+function compareDates(a, b) {
+  a = new Date(a.date);
+  b = new Date(b.date);
+  return a.getTime() - b.getTime();
+  }
+
+function nextUrgent() {
+  const tasksWithDateObjects = tasks.map(task => ({
+    ...task,
+    date: new Date(task.date)
+  }));
+  const urgentTasks = tasksWithDateObjects.filter(task => task.priority === "Urgent");
+  urgentTasks.sort(compareDates);
+  const nextUrgentDate = urgentTasks[0].date;
+  const formattedDate = nextUrgentDate.toLocaleDateString("de-DE", { year: "numeric", month: "long", day: "numeric" });
+  const urgentDate = document.getElementById("urgentDate");
+  urgentDate.textContent = `${formattedDate}`;
 }
