bd65801f60a4f824920c051a6e65adff866cfcb4
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-03-30 11:20:46 +0100

    add zIndex d&d cursor

--- js/dragAndDrop.js
+++ js/dragAndDrop.js
@@ -2,7 +2,8 @@ let elementToDrag;
 
 function startDragging(id) {
     elementToDrag = id;
-    // document.getElementById(`todobox${id}`).style.transform = 'rotate(5deg)'
+    document.getElementById(`todobox${id}`).style.zIndex = 100;
+    document.getElementById(`todobox${id}`).style.backgroundColor = 'rgba(255, 255, 255, 1)';
 }
 
 function allowDrop(event) {
