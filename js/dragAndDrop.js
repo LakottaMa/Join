f38705754f69224f188086e802979e40e06376c2
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-04-05 13:32:22 +0200

    fix width from add task mobile in board, comments in drag and drop js file

--- js/dragAndDrop.js
+++ js/dragAndDrop.js
@@ -2,7 +2,6 @@ let elementToDrag;
 
 function startDragging(id) {
     elementToDrag = id;
-    document.getElementById(`todobox${id}`).style.transform = 'rotate(5deg)';
 }
 
 function allowDrop(event) {
@@ -25,7 +24,11 @@ function removeHighlight(id) {
 // cursor above
 
 // touch below
+
 let all;
+/**
+ * start touch events for all tasks with class todoBox
+ */
 function startTouchEvents() {
     all = document.querySelectorAll('.todoBox');
     all.forEach(addStart);
@@ -36,11 +39,18 @@ const progress = document.getElementById('inProgressContainer');
 const feedback = document.getElementById('awaitFeedbackContainer');
 const done = document.getElementById('doneContainer');
 
+/**
+ * hightlight the area for touchend
+ * @param {string} area 
+ */
 function highlightArea(area) {
     removeAllHighlight();
     area.classList.add('dragAreaHighlight');
 }
 
+/**
+ * remove highligt from all areas
+ */
 function removeAllHighlight() {
     todo.classList.remove('dragAreaHighlight');
     progress.classList.remove('dragAreaHighlight');
@@ -53,6 +63,9 @@ let progressPos = progress.getBoundingClientRect();
 let feedbackPos = feedback.getBoundingClientRect();
 let donePos = done.getBoundingClientRect();
 
+/**
+ * update the position from the target areas
+ */
 function updatePositions() {
     todoPos = todo.getBoundingClientRect();
     progressPos = progress.getBoundingClientRect();
@@ -60,72 +73,115 @@ function updatePositions() {
     donePos = done.getBoundingClientRect();
 }
 
-function addStart(elem) {
-    let id = elem.id.slice(-1);
-    let taskToMove = tasks[id];
-    elem.addEventListener('touchstart', e => {
-        let startX = e.changedTouches[0].clientX;
-        let startY = e.changedTouches[0].clientY;
-        updatePositions();
-        let offsetX = elem.offsetLeft;
-        let offsetY = elem.offsetTop;        
-        let touchMoveEnabled = false;
-        let touchMoved = false;        
-        e.preventDefault();
-        let timeoutID = setTimeout(() => {
-            touchMoveEnabled = true;
-            elem.style.transform = 'rotate(5deg)';
-        }, '500');
-        elem.addEventListener('touchmove', eve => {           
-            eve.preventDefault();
-            if (touchMoveEnabled) {
-                touchMoved = true;
-                let nextX = eve.changedTouches[0].clientX;
-                let nextY = eve.changedTouches[0].clientY;
-                elem.style.position = 'absolute';
-                elem.style.left = (nextX - startX + offsetX) + 'px';
-                elem.style.top = (nextY - startY + offsetY) + 'px';
-                elem.style.zIndex = 15;
-                if (isElementInside(progressPos, elem)) {
-                    highlightArea(progress);
-                } else if (isElementInside(feedbackPos, elem)) {
-                    highlightArea(feedback);
-                } else if (isElementInside(donePos, elem)) {
-                    highlightArea(done);
-                } else if (isElementInside(todoPos, elem)) {
-                    highlightArea(todo);
-                } else {
-                    removeAllHighlight();
-                }
-            }
-        });
+let startX; // Start Position in X
+let startY; // Start Position in Y
+let offsetX;    // offset X wird für position absolute benötigt
+let offsetY;    // offset Y wird für position absolute benötigt
+let touchMoveEnabled = false;   // boolean wird true, wenn 500ms vom timeout vergangen sind
+let touchMoved = false; // boolean wird true, wenn der Task bewegt wurde
+let timeoutID;  // timeoutID wird bei touchend zurück gesetzt
 
+/**
+ * activate event listener for touch events
+ * @param {*} elem 
+ */
+function addStart(elem) {
+    let id = elem.id.slice(-1); // id for the task
+    let taskToMove = tasks[id]; // movable task
+    elem.addEventListener('touchstart', e => {
+        handleTouchStart(elem, e);
+        elem.addEventListener('touchmove', eve => {
+            handleTouchMove(eve, elem);
+        });
         elem.addEventListener('touchend', eve => {
-            clearTimeout(timeoutID);
-            elem.style.zIndex = 0;
-            if (!touchMoveEnabled && !touchMoved) {
-                showDetailBox(id);
-            } else {
-                if (isElementInside(progressPos, elem)) {
-                    dropElementInDiv(elem, 'In progress', taskToMove);
-                } else if (isElementInside(feedbackPos, elem)) {
-                    dropElementInDiv(elem, 'Await feedback', taskToMove);
-                } else if (isElementInside(donePos, elem)) {
-                    dropElementInDiv(elem, 'Done', taskToMove)
-                } else if (isElementInside(todoPos, elem)) {
-                    dropElementInDiv(elem, 'ToDo', taskToMove);
-                } else {
-                    resetElement(elem);
-                    hideDetailBox();
-                }
-            }
-            touchMoveEnabled = false;
-            touchMoved = false;
+            handleTouchEnd(elem, id, taskToMove);
         });
     });
 }
 
+/**
+ * set startpoints and offset coordinates, set timeout and rotate task after timeout
+ * @param {*} elem 
+ * @param {*} e 
+ */
+function handleTouchStart(elem, e) {
+    startX = e.changedTouches[0].clientX;   // X-Position vom ersten Touch
+    startY = e.changedTouches[0].clientY;   // Y-Position vom ersten Touch
+    updatePositions();
+    offsetX = elem.offsetLeft;              // Entfernung zum nächsten linken Rand des parent Elements
+    offsetY = elem.offsetTop;               // Entfernung zum nächsten oberen Rand des parent Elements
 
+    e.preventDefault();
+    timeoutID = setTimeout(() => {
+        touchMoveEnabled = true;
+        elem.style.transform = 'rotate(5deg)';
+    }, '500');
+}
+
+/**
+ * set the coordinates for the moving task
+ * @param {*} eve 
+ * @param {*} elem 
+ */
+function handleTouchMove(eve, elem) {
+    eve.preventDefault();
+    if (touchMoveEnabled) {
+        touchMoved = true;
+        let nextX = eve.changedTouches[0].clientX;  // X-Position vom ersten Touch
+        let nextY = eve.changedTouches[0].clientY;  // Y-Position vom ersten Touch
+        elem.style.position = 'absolute';           // position: absolut, damit der Task über allen anderen ist
+        elem.style.left = (nextX - startX + offsetX) + 'px';    // Berechnung der neuen Position als style left
+        elem.style.top = (nextY - startY + offsetY) + 'px';     // Berechnung der neuen Position als style top
+        elem.style.zIndex = 15;
+        if (isElementInside(progressPos, elem)) {               // Überprüfung ob der Task über einem Target ist
+            highlightArea(progress);                            // highlight area 
+        } else if (isElementInside(feedbackPos, elem)) {
+            highlightArea(feedback);
+        } else if (isElementInside(donePos, elem)) {
+            highlightArea(done);
+        } else if (isElementInside(todoPos, elem)) {
+            highlightArea(todo);
+        } else {
+            removeAllHighlight();
+        }
+    }
+}
+
+/**
+ * open detailBox or move task to new area
+ * @param {*} elem 
+ * @param {*} id 
+ * @param {*} taskToMove 
+ */
+function handleTouchEnd(elem, id, taskToMove) {
+    clearTimeout(timeoutID);                        // timeout zurück setzen
+    elem.style.zIndex = 0;
+    if (!touchMoveEnabled && !touchMoved) {         // Wenn touchmove false und touchMoved false, dann
+        showDetailBox(id);                          // DetailView öffnen
+    } else {                                        // Wenn nicht, Task verschieben
+        if (isElementInside(progressPos, elem)) {
+            dropElementInDiv(elem, 'In progress', taskToMove);
+        } else if (isElementInside(feedbackPos, elem)) {
+            dropElementInDiv(elem, 'Await feedback', taskToMove);
+        } else if (isElementInside(donePos, elem)) {
+            dropElementInDiv(elem, 'Done', taskToMove)
+        } else if (isElementInside(todoPos, elem)) {
+            dropElementInDiv(elem, 'ToDo', taskToMove);
+        } else {
+            resetElement(elem);
+            hideDetailBox();
+        }
+    }
+    touchMoveEnabled = false;
+    touchMoved = false;
+}
+
+/**
+ * checking whether the element is in an area
+ * @param {*} container 
+ * @param {*} element 
+ * @returns 
+ */
 function isElementInside(container, element) {
     if (getCenterX(element) > container.left && getCenterX(element) < container.right && getCenterY(element) > container.top && getCenterY(element) < container.bottom) {
         return true;
@@ -134,6 +190,10 @@ function isElementInside(container, element) {
     }
 }
 
+/**
+ * reset Element and start touchEvents again
+ * @param {*} elem 
+ */
 function resetElement(elem) {
     resetElementPos(elem);
     elem.style.transform = 'rotate(0deg)';
@@ -141,6 +201,12 @@ function resetElement(elem) {
     startTouchEvents();
 }
 
+/**
+ * move task to new area
+ * @param {*} elem 
+ * @param {*} status 
+ * @param {*} taskToMove 
+ */
 function dropElementInDiv(elem, status, taskToMove) {
     taskToMove['status'] = status;
     resetElementPos(elem);
@@ -149,18 +215,32 @@ function dropElementInDiv(elem, status, taskToMove) {
     startTouchEvents();
 }
 
+/**
+ * 
+ * @param {*} element 
+ * @returns Center Position in X
+ */
 function getCenterX(element) {
     let rect = element.getBoundingClientRect();
     let centerX = rect.left + (rect.width / 2);
     return centerX;
 }
 
+/**
+ * 
+ * @param {*} element 
+ * @returns Center Position in Y
+ */
 function getCenterY(element) {
     let rect = element.getBoundingClientRect();
     let centerY = rect.top + (rect.height / 2);
     return centerY;
 }
 
+/**
+ * reset Task back to Position: static and left and top 0
+ * @param {*} elem 
+ */
 function resetElementPos(elem) {
     elem.style.position = 'static';
     elem.style.left = 0 + "px";
